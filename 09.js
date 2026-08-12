436308e2faa57fe5834c13d4eb388b2f9ab886ae
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2026-08-12 19:38:22 +0600

    added 09.js

---
+++ 09.js
@@ -0,0 +1,12 @@
+function  splitFirstRest(arr){
+
+    const [first, ...rest]  = arr;
+
+    return {
+        first: first,
+        rest: rest
+    }
+
+}
+
+console.log(splitFirstRest([10, 20, 30, 40]));
